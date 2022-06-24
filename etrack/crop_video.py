4a38edfb7159359f2e93bb3b715745369a93a591
Xaver Roos <xaver.roos@gmail.com>
2022-06-24 15:19:47 +0200

    [crop_video] argsparse working

--- etrack/crop_video.py
+++ etrack/crop_video.py
@@ -16,8 +16,10 @@ class CropVideo():
         super().__init__()
 
         self._file_name = file_name
-        self.cut_out_video
         self.mark_crop_positions
+        self.cut_out_video
+        self.plot_video
+        self.crop_frame
 
 
     def mark_crop_positions(self, file_name, frame_number):
@@ -52,43 +54,85 @@ class CropVideo():
         # embed()
         # quit()
         return result
-  
+    
+
+    def crop_frame(self, frame, marker_positions):
+
+        # load the four marker positions 
+        bottom_left_x, bottom_left_y, bottom_right_x, bottom_right_y, top_left_x, top_left_y, top_right_x, top_right_y = assign_marker_positions(marker_positions)
+
+        # define boundaries of frame, taken by average of points on same line but slightly different pixel values
+        left_bound = int(np.mean([bottom_left_x, top_left_x]))
+        right_bound = int(np.mean([bottom_right_x, top_right_x]))
+        top_bound = int(np.mean([top_left_y, top_right_y]))
+        bottom_bound = int(np.mean([bottom_left_y, bottom_right_y]))
+        
+        # crop the frame by boundary values
+        cropped_frame = frame[top_bound:bottom_bound, left_bound:right_bound]
+        # cropped_frame = np.mean(cropped_frame, axis=2)    # mean over 3rd dimension (RGB/color values)
+
+        return cropped_frame
+
+
+    def plot_video(self, filename, frame_number, marker_positions):
+        if not os.path.exists(filename):
+            raise IOError("file %s does not exist!" % filename)
+        video = cv2.VideoCapture()
+        video.open(filename)
+        frame_counter = 0
+        success = True
+        frame = None
+        while success and frame_counter <= frame_number:    # iterating until frame_counter == frame_number --> success (True)
+            print("Reading frame: %i" % frame_counter, end="\r")
+            success, frame = video.read()
+            frame_counter += 1
+        if success:
+            cropped_frame = cv.crop_frame(frame, marker_positions)  
+            
+            fig, ax = plt.subplots()
+            ax.imshow(cropped_frame)    # plot wanted frame of video
+        else:
+           print("Could not read frame number %i either failed to open movie or beyond maximum frame number!" % frame_number)
+           return []
+        plt.title(filename)
+        plt.show(block=True)
+        
+        return filename
+
 
 if __name__ == "__main__":    
     
     parser = argparse.ArgumentParser(description='Crop video to wanted pixel parameters')
     parser.add_argument('-p', action='store_true', help='plot the video to check fit of cropping parameters')
     parser.add_argument('-c', action='store_true', help='set cropping parameters manually for each video')
-    parser.add_argument('-d', type=str, help='destination folder for cropped videos')    # const='/home/efish/etrack/cropped_videos/', 
-    parser.add_argument('-f', type=int, help='frame number to plot, default=10')    #  const='10',
+    parser.add_argument('-d', type=str, help='destination folder for cropped videos', default='/home/efish/etrack/cropped_videos/') 
+    parser.add_argument('-f', type=int, help='frame number to plot, default=10', default=10)
     args = parser.parse_args()
 
-    print(args.p)
     for enu, file_name in enumerate(glob.glob("/home/efish/etrack/videos/*")): 
-        # if file_name == '/home/efish/etrack/videos/2022.03.28_7.mp4':
         print(file_name)
-        cv = CropVideo(file_name=file_name)
-        
-        frame_number = 10
-        destination_folder = "/home/efish/etrack/cropped_videos/"
-        # if args.d or args.f != None:
-        #     frame_number = args.f
-        #     destination_folder = args.d
+        cv = CropVideo(file_name)
 
-        print(enu)
-        if enu == 0 or args.c:
-            marker_positions = cv.mark_crop_positions(file_name, frame_number)
-        elif args.p:
-            file_name = plot_video(file_name, frame_number)
+        if enu == 0:    # first run always cropping to get first marker positions
+            marker_positions = cv.mark_crop_positions(file_name, args.f)
+            print('enu=0')
             
-        else:
-            pass
-
-        print('marker positions done')
+        else:   # for each other file
+            if args.c and args.p == True:   # crop positions and plotting
+                marker_positions = cv.mark_crop_positions(file_name, args.f)
+                file_name = cv.plot_video(file_name, args.f, marker_positions)
+            elif args.c == False and args.p == True:    # only plotting
+                file_name = cv.plot_video(file_name, args.f, marker_positions)
+            elif args.c == True and args.p == False:    # only crop positions
+                marker_positions = cv.mark_crop_positions(file_name, args.f)
+            else:
+                pass
+        
         bottom_left_x = int(marker_positions[0]['bottom left corner'][0]) 
         bottom_left_y = int(marker_positions[0]['bottom left corner'][1])
         top_right_x= int(marker_positions[0]['top right corner'][0])
         top_right_y = int(marker_positions[0]['top right corner'][1])
 
-        result = cv.cut_out_video(file_name, destination_folder, (bottom_left_x, bottom_left_y), (top_right_x, top_right_y))
-        print(result)
+        result = cv.cut_out_video(file_name, args.d, (bottom_left_x, bottom_left_y), (top_right_x, top_right_y))    # actual cropping of video
+        
+        
