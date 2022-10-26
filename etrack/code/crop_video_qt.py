c33f2a0242bc798f5376d8a31dea2820c4965d5d
xaveros <xaver.roos@gmail.com>
2022-10-26 14:07:10 +0200

    [crop_video_qt] File Selector and 3 Buttons  Video Tools working

--- etrack/code/crop_video_qt.py
+++ etrack/code/crop_video_qt.py
@@ -43,13 +43,6 @@ class FileSelector(QWidget):
         
         self.display_item_list = QListWidget()
         self.display_item_list.setMaximumHeight(400)
-        
-        self.list_items = []
-        for i in range(self.display_item_list.count()): 
-            item = self.display_item_list.item(i).text()
-            if item not in self.list_items:
-                self.list_items.append(self.display_item_list.item(i).text())
-        
         return self.display_item_list
 
 
@@ -87,28 +80,23 @@ class FileSelector(QWidget):
 
     def destination_box(self):
         self.destination = QListWidget()
+        self.default_path = '/home/student/etrack/cropped_videos'
+        self.destination.addItem(self.default_path)
         self.destination.setFixedHeight(30)
         return self.destination
 
 
     def browse_group(self):
-        groupBox = QGroupBox()
-        browseButton = QPushButton('browse')
-        browseButton.setFixedHeight(30)
-        
-        self.destination_list = []
-        
-        browseButton.clicked.connect(self.browse_file)
-        
-        vbox = QVBoxLayout()
-        vbox.addWidget(browseButton)
-        groupBox.setLayout(vbox)
-     
-        return groupBox
+        self.browseButton = QPushButton('browse')
+                
+        self.browseButton.clicked.connect(self.browse_file)     
+        return self.browseButton
 
 
     def browse_file(self):
-        self.path = str(QFileDialog.getExistingDirectory(self, "Select Directory", '/home/student/etrack/cropped_videos'))
+        self.destination.clear()
+        self.destination_list = []
+        self.path = str(QFileDialog.getExistingDirectory(self, "Select Directory", self.default_path))
         self.destination.addItem(self.path)
         if len(self.destination) > 1:
             self.destination.clear()
@@ -119,16 +107,15 @@ class FileSelector(QWidget):
     def frame_spinbox(self):
         groupBox = QGroupBox()
 
-        frame_spinbox = QSpinBox()
-        frame_spinbox.setValue(10)
+        self.frame_spinbox = QSpinBox()
+        self.frame_spinbox.setValue(10)
         
-        print("current value:"+str(frame_spinbox.value()))
-        self.spinbox_value = frame_spinbox.value()
+        print("current value:"+str(self.frame_spinbox.value()))
+        self.spinbox_value = self.frame_spinbox.value()
 
         vbox = QVBoxLayout()
-        vbox.addWidget(frame_spinbox)
+        vbox.addWidget(self.frame_spinbox)
         groupBox.setLayout(vbox)
-
         return groupBox
 
 
@@ -145,79 +132,106 @@ class VideoTools(QWidget):
         
         # how to define them for the whole class?
         self.frame_number = self.file_selector.spinbox_value
-        self.file_list = self.file_selector.item_list
+        self.file_list = self.file_selector.display_item_list
+        print(len(self.file_list))
+        self.destination = self.file_selector.destination
         
-
         hl = QHBoxLayout()
         
-        mark_crop_positions_btn = QPushButton('mark crop positions')
-        mark_crop_positions_btn.clicked.connect(self.mark_crop_positions)
-
-        plot_btn = QPushButton('plot frame')
-        # plot_btn.setCheckable(True)
-        plot_btn.clicked.connect(self.plot_frame)
-
-        crop_btn = QPushButton('crop video')
-        crop_btn.clicked.connect(self.crop_video)
+        self.mark_crop_positions_btn = QPushButton('mark crop positions')
+        self.mark_crop_positions_btn.clicked.connect(self.mark_crop_positions)
+        self.mark_crop_positions_btn.clicked.connect(self.mark_crop_positions_done)
         
-        parameter_btn = QPushButton('print parameter')
-        parameter_btn.clicked.connect(self.print_parameter)
-        
-        set_crop_btn = QPushButton('set crop parameter')
-        set_crop_btn.clicked.connect(self.set_crop_parameter)
+        # does not get updated when status of len(self.file_list) does change..
+        # if len(self.file_list) == 0:
+        #     self.mark_crop_positions_btn.setEnabled(False)
 
-        hl.addWidget(mark_crop_positions_btn)
-        hl.addWidget(plot_btn)
-        hl.addWidget(crop_btn)
-        hl.addWidget(parameter_btn)
-        hl.addWidget(set_crop_btn)
+        self.plot_btn = QPushButton('plot frame')
+        self.plot_btn.setEnabled(False)
+        self.plot_btn.clicked.connect(self.plot_frame)
+
+        self.crop_btn = QPushButton('crop video')
+        self.crop_btn.setEnabled(False)
+        self.crop_btn.clicked.connect(self.crop_video)
+        
+        self.parameter_btn = QPushButton('print parameter')
+        self.parameter_btn.setEnabled(False)
+        self.parameter_btn.clicked.connect(self.print_parameter)
+        
+        self.set_crop_btn = QPushButton('set crop parameter')
+        self.set_crop_btn.setEnabled(False)
+        self.set_crop_btn.clicked.connect(self.set_crop_parameter)
+
+        hl.addWidget(self.mark_crop_positions_btn)
+        hl.addWidget(self.plot_btn)
+        hl.addWidget(self.crop_btn)
+        hl.addWidget(self.parameter_btn)
+        hl.addWidget(self.set_crop_btn)
 
         self.setLayout(hl)
 
 
+    def mark_crop_positions_done(self):
+        self.plot_btn.setEnabled(True)
+        self.crop_btn.setEnabled(True)
+        self.parameter_btn.setEnabled(True)
+        self.set_crop_btn.setEnabled(True)
+
+
     def mark_crop_positions(self):  # set marker for first (!) video, used for all following videos    
-        self.crop_positions = CropVideo.parser_mark_crop_positions(self.file_list[0], self.frame_number)
+
+        self.list_items = []    # needed as variable for other class
+        for i in range(self.file_list.count()): 
+            item = self.file_list.item(i).text()
+            if item not in self.list_items:
+                self.list_items.append(self.file_list.item(i).text())
+                print('display item list', self.file_list.item(i).text())
+
+        # not working with file list cause empty, import from other class not really working
+        self.crop_positions = CropVideo.parser_mark_crop_positions(self.list_items[0], self.frame_number)
+        
+        return self.crop_positions
 
 
-    def plot_frame(self):   # check how croppping for the different videos would look like
+    def plot_frame(self):
         self.cropped_frame_list = []
-        embed()
-        quit()
-        for file in self.file_list:
+        for file in self.list_items:
             cropped_frame = CropVideo.parser_plot_frame(file, self.frame_number, self.crop_positions)
             self.cropped_frame_list.append(cropped_frame)
 
         # source: https://engineeringfordatascience.com/posts/matplotlib_subplots/
         ncols = 3
-        nrows = len(self.cropped_frame_list) // ncols + (len(self.cropped_frame_list) % ncols > 0)  # self adjusting row number depending on number of videos
+        # calculate number of rows
+        nrows = len(self.cropped_frame_list) // ncols + (len(self.cropped_frame_list) % ncols > 0)
 
         plt.subplots_adjust(hspace=0.3)
         plt.suptitle("plot frame of videos, frame number = %i" % self.frame_number, fontsize=12)
-        
-        for n, (frame, filename) in enumerate(zip(self.cropped_frame_list, self.file_list)):
-            ax = plt.subplot(nrows, ncols, n + 1)   # add subplot for each video/frame
-          
-            ax.imshow(frame)    # plot frame
-          
+
+        for n, (frame, filename) in enumerate(zip(self.cropped_frame_list, self.list_items)):
+            # add a new subplot iteratively using nrows and cols
+            ax = plt.subplot(nrows, ncols, n + 1)
+
             filename = filename.split('/')[-1]
+            ax.imshow(frame)
             ax.set_title(filename, fontsize=11)
+
         plt.show()
-        
+
 
     def crop_video(self):
         print('crop video')
-
-        # file list empty..
         
-        for file in self.file_list:
-            marker_crop_positions = CropVideo.parser_mark_crop_positions(file, self.frame_number)
+        marker_crop_positions = self.crop_positions
+        
+        for file in self.list_items:
             
             bottom_left_x, bottom_left_y, bottom_right_x, bottom_right_y, top_left_x, top_left_y, top_right_x, top_right_y = assign_marker_positions(marker_crop_positions)
             result = CropVideo.parser_cut_out_video_parser(file, str(self.destination.item(0).text()), (bottom_left_x, bottom_left_y), (top_right_x, top_right_y))  
-            print(self.file_list)
+            
 
     def print_parameter(self):
         print('print parameter')
+        # first making an terminal with corresponding output?
     
 
     def set_crop_parameter(self):
@@ -232,14 +246,29 @@ class MainWindow(QMainWindow):
         
         self.fs = FileSelector()
         self.vt = VideoTools(self.fs)
-    
+
         layout = QGridLayout()
-        layout.addWidget(self.fs,)
+
+        file_selector_label = QLabel('File Selector Tools:')
+        layout.addWidget(file_selector_label)
+        
+        layout.addWidget(self.fs)
+
+        video_tools_label = QLabel('Video Tools:')
+        layout.addWidget(video_tools_label)
+
         layout.addWidget(self.vt)
+
+        layout.setSpacing(20)
         
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
+
+
+
+
+
         
 
 app = QApplication(sys.argv)
